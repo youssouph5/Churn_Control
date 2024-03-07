79364009c1958d649434c4869e22117c35e7db41
youssouph5 <103521013+youssouph5@users.noreply.github.com>
2024-03-07 12:46:38 +0100

    Update streamlit-Churn.py

--- streamlit-Churn.py
+++ streamlit-Churn.py
@@ -130,7 +130,7 @@ def perform_prediction(df_pred):
 
     # Télécharger le modèle depuis GitHub
     #model_url = "https://raw.githubusercontent.com/VotreNom/VotreRepo/main/model.joblib" "https://github.com/youssouph5/Churn_Control/raw/main/Model_Churn151223.joblib"
-    model_url = "https://github.com/youssouph5/Churn_Control/raw/main/Model_Churn151223.joblib"
+    model_url = "https://github.com/youssouph5/Churn_Control/blob/main/Model_Churn151223.joblib"
     response = requests.get(model_url)
     model_file = BytesIO(response.content)
 
